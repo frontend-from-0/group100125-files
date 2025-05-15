6ba5026e26445fb1dbe530c992605c9fc130eb2f
code2career <info@annaatalay.com>
2025-05-15 21:08:04 +0200

    Add Lesson17 completed files

--- Lesson17/script.js
+++ Lesson17/script.js
@@ -21,10 +21,16 @@ You'll practice:
 2. Each flashcard is an object with 'question' and 'answer'.
 */
 
-  // 'What is HTML?'
-  // 'A language for making web pages'
-  // 'What does CSS do?'
-  // 'Styles the web page'
+const flashcards = [
+  {
+    question: 'What is HTML?',
+    answer: 'A language for making web pages',
+  },
+  {
+    question: 'What does CSS do?',
+    answer: 'Styles the web page',
+  },
+];
 
 /*
 -----------------------------------------------------------
@@ -33,9 +39,16 @@ You'll practice:
 Function: showAllFlashcards()
 - Loops over the 'flashcards' array.
 - Logs the question and answer for each card.
+- HOMEWORK: Make questions equal in length so it's easier to read output
 */
-
-
+function showAllFlashcards() {
+  console.log('All available flashcard:');
+  for (const flashcard of flashcards) {
+    console.log(`${flashcard.question} --- ${flashcard.answer}`);
+  }
+  console.log();
+}
+showAllFlashcards();
 
 /*
 -----------------------------------------------------------
@@ -46,7 +59,30 @@ Function: addFlashcard(question, answer)
 - Checks if a flashcard with the same question already exists.
 - Logs a message if added or if already exists.
 */
+function addFlashcard(question, answer) {
+  console.log();
+  console.log('Adding a new flashcards...');
 
+  const flashcard = {
+    question: question,
+    answer: answer,
+  }; // is the same as: const flashcard = {question, answer};
+
+  for (const card of flashcards) {
+    if (card.question === question) {
+      console.log(`Flashcard with question \'${question}\' already exists`);
+      return;
+    }
+  }
+  flashcards.push(flashcard);
+  console.log(`Added a new flashcard with question: \'${question}\'`);
+  console.log();
+}
+
+addFlashcard('What is HTML?', 'A language for making web pages');
+addFlashcard('What is JS?', 'A programming language');
+
+showAllFlashcards();
 
 /*
 -----------------------------------------------------------
@@ -60,7 +96,21 @@ Function: practiceFlashcard(index)
 NOTE: For simplicity, we'll just log both.
 */
 
+function practiceFlashcard(index) {
+  if (index >= 0 && index < flashcards.length) {
+    console.log('Question: ', flashcards[index].question);
+    setTimeout(
+      () => console.log('The answer is:', flashcards[index].answer)
+    , 3000);
 
+  } else {
+    console.log(`Flashcard with index ${index} is not in the list. Use index from 0 to ${flashcards.length - 1}`);
+  }
+}
+
+practiceFlashcard(2);
+console.log('---');
+practiceFlashcard(5);
 /*
 -----------------------------------------------------------
   STEP 5: Update a Flashcard
@@ -69,7 +119,6 @@ Function: updateFlashcard(question, newQuestion, newAnswer)
 - Finds flashcard by question and updates it.
 - Logs if updated or not found.
 */
-
 
 /*
 -----------------------------------------------------------
@@ -80,15 +129,12 @@ Function: removeFlashcard(question)
 - Logs if removed or not found.
 */
 
-
 /*
 -----------------------------------------------------------
   STEP 7: Test the App
 -----------------------------------------------------------
 Use these function calls to test your app.
 */
-
-
 
 /*
 -----------------------------------------------------------
