0fef754415bc538391a52d0f4253a8de469a4be0
annasvst <annaatalay@hotmail.com>
2025-09-25 19:34:47 +0200

    Update starter files for lesson 33

--- Lesson33/random-quotes/src/components/Nav.jsx
+++ Lesson33/random-quotes/src/components/Nav.jsx
@@ -1,29 +1,12 @@
 import Link from 'next/link';
-import { ModeToggle } from '@/components/ModeToggle';
-import {
-  NavigationMenu,
-  NavigationMenuList,
-  NavigationMenuItem,
-  NavigationMenuLink,
-} from './ui/navigation-menu';
 
 
 export const Navbar = () => {
   return (
-    <NavigationMenu className='bg-gray-200 dark:bg-gray-900'>
-      <NavigationMenuList className={'flex gap-4 justify-end w-screen px-16 py-4'}>
-        <NavigationMenuItem >
-          <NavigationMenuLink asChild>
-            <Link href='/'>Home</Link>
-          </NavigationMenuLink>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-          <NavigationMenuLink asChild>
-            <Link href='/admin/'>Admin</Link>
-          </NavigationMenuLink>
-        </NavigationMenuItem>
-        <ModeToggle />
-      </NavigationMenuList>
-    </NavigationMenu>
+
+    <nav className='flex gap-4'>
+      <Link href='/'>Home</Link>
+      <Link href='/admin/'>Admin</Link>
+    </nav>
   );
 };
